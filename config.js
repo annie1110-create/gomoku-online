3a0e66c035e54e80f2b5e6acb96d1d7ae4fc1b7c
annie1110-create <annie0931110@gmail.com>
2026-06-02 21:06:52 +0800

    Add Firebase configuration template

---
+++ config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "你的APIKEY",
+  authDomain: "你的authDomain",
+  databaseURL: "你的databaseURL",
+  projectId: "你的projectId",
+  storageBucket: "你的storageBucket",
+  messagingSenderId: "你的messagingSenderId",
+  appId: "你的appId"
+};
